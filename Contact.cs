974aeb995f96eafec1fca5608e681cb02835abed
Sinchana41 <sinchanaan1234@gmail.com>
2025-12-21 19:24:21 +0530

    create contacts on addressbook

---
+++ Contact.cs
@@ -0,0 +1,31 @@
+﻿namespace AddressBookSystem.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Contact other)
+                return FirstName.Equals(other.FirstName);
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return FirstName.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}, {City}, {State}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AddressBookSystem.Models;
+using System;
 
 namespace AddressBookSystem
 {
@@ -6,7 +7,18 @@ namespace AddressBookSystem
     {
         static void Main()
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            Contact contact = new Contact();
+            contact.FirstName = "Sinchana";
+            contact.LastName = "A N";
+            contact.Address = "Anuvanahalli";
+            contact.City = "Ajjampura";
+            contact.State = "Karnataka";
+            contact.Zip = "577549";
+            contact.Email = "sin@gmail.com";
+            contact.Phone = "2345678911";
+
+            Console.WriteLine(contact);
+
         }
     }
 }
