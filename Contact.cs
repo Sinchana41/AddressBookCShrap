d2fe7532ae4e40e144b84caba384199c644e935f
Sinchana41 <sinchanaan1234@gmail.com>
2025-12-21 21:46:26 +0530

    Chech duplicate contacts in same address book

--- Contact.cs
+++ Contact.cs
@@ -1,4 +1,4 @@
-﻿namespace AddressBookSystem.Models
+﻿namespace AddressBookSystems
 {
     public class Contact
     {
@@ -11,21 +11,29 @@
         public string Phone { get; set; }
         public string Email { get; set; }
 
+        // UC-7 CORE LOGIC
         public override bool Equals(object obj)
         {
-            if (obj is Contact other)
-                return FirstName.Equals(other.FirstName);
-            return false;
+            if (obj == null || !(obj is Contact))
+                return false;
+
+            Contact other = (Contact)obj;
+
+            return this.FirstName.Equals(other.FirstName, StringComparison.OrdinalIgnoreCase)
+                && this.LastName.Equals(other.LastName, StringComparison.OrdinalIgnoreCase);
         }
 
         public override int GetHashCode()
         {
-            return FirstName.GetHashCode();
+            return HashCode.Combine(
+                FirstName.ToLower(),
+                LastName.ToLower()
+            );
         }
 
         public override string ToString()
         {
-            return $"{FirstName} {LastName}, {City}, {State}";
+            return $"{FirstName} {LastName}, {City}, {State}, {Zip}, {Phone}, {Email}";
         }
     }
 }
